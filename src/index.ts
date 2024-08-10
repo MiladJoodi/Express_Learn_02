3e1aa2a6989d928216d6618d11c94b25abe3d01a
Milad Joodi <winbeta.com@gmail.com>
2024-08-10 23:00:48 +0330

    up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+import { Request, Response } from "express";
 
-app.get("/", (req, res)=>{
+app.get("/", (req: Request, res:Response)=>{
     res.send("Hello world")
 })
 
