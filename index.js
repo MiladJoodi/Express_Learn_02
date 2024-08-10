04f1dc1ec5bac89f12a6d010b8cfa5bca06fd4eb
Milad Joodi <winbeta.com@gmail.com>
2024-08-10 14:42:04 +0330

    Nodemon

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("Hello world")
+})
+
+app.get("/users", (req, res)=>{
+    const users = [
+        {name: "john", age: 20},
+        {name: "Smith", age: 27},
+        {name: "Eliza", age: 25}
+    ]
+    res.send(users)
+
+})
+
+app.listen(3000, ()=>(
+    console.log("Server is running on port 3000")
+))
