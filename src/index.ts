147bba56c7fea30b1643cbc0389fd6ae1f9328cb
Milad Joodi <winbeta.com@gmail.com>
2024-08-10 23:29:51 +0330

    middleware

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,32 @@
+import { NextFunction, Request, Response } from "express";
 const express = require('express');
 const app = express();
-import { Request, Response } from "express";
+
+
+// middleware
+const myMiddleware = ((req: Request,res:Response, next: NextFunction)=>{
+    console.log("time: ", Date.now());
+    next();
+})
+
+
 
 app.get("/", (req: Request, res:Response)=>{
+    console.log(req.body)
     res.send("Hello world")
 })
 
-app.get("/users", (req, res)=>{
-    const users = [
-        {name: "john", age: 20},
-        {name: "Smith", age: 27},
-        {name: "Eliza", age: 25}
-    ]
-    res.send(users)
 
-})
+
+// app.get("/users", (req, res)=>{
+//     const users = [
+//         {name: "john", age: 20},
+//         {name: "Smith", age: 27},
+//         {name: "Eliza", age: 25}
+//     ]
+//     res.send(users)
+
+// })
 
 app.listen(3000, ()=>(
     console.log("Server is running on port 3000")
